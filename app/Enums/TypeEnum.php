b762c38afe660a356f2da461a39acac2e950f109
ZvonLexa <zvon.lexa@yandex.ru>
2025-02-26 11:24:20 +0300

    create migrate characteristic

---
+++ app/Enums/TypeEnum.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enums;
+
+use App\Contracts\HasEnumName;
+use App\Enums\Traits\EnumValue;
+use App\Enums\Traits\EnumName;
+
+enum TypeEnum: int implements HasEnumName
+{
+  use EnumValue;
+
+  case Boolean = 1;
+  case Integer = 2;
+  case Brand = 3;
+  case Color = 4;
+  case Dimension = 5;
+
+  public static function getEnumName(): string
+  {
+      return TypeNameEnum::class;
+  }
+}
+
+enum TypeNameEnum: string
+{
+    use EnumName;
+
+    case Boolean = 'Булево';
+    case Integer = 'Число';
+    case Brand = 'Брэнд';
+    case Color = 'Цвет';
+    case Dimension = 'Размер';
+}
+
+enum TypeTableEnum: string
+{
+  use EnumName;
+
+  case Boolean = '';
+  case Integer = '';
+  case Brand = 'brands';
+  case Color = 'colors';
+  case Dimension = 'dimensions';
+}
---
+++ database/migrations/2025_02_26_080221_create_characteristics_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Category;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('characteristics', function (Blueprint $table) {
+            $table->id();
+            $table->nestedSet(); // Добавляет поля _lft, _rgt, parent_id
+            $table->foreignIdFor(Category::class)->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->boolean('smart')->default(false);
+            $table->unsignedInteger('sort')->default(100);
+            $table->string('title');
+            $table->integer('type')->default(1);
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('characteristics');
+    }
+};
