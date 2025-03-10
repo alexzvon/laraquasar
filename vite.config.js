cb80370dac7e30e0112bda35f4eb29f64791153a
ZvonLexa <zvon.lexa@yandex.ru>
2025-03-10 13:54:35 +0300

    fix 10032025

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-// import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 import { quasar } from "@quasar/vite-plugin";
 
 export default defineConfig({
@@ -24,13 +24,6 @@ export default defineConfig({
             sassVariables: fileURLToPath(
               new URL('./resources/css/quasar-variables.sass', import.meta.url)
             )
-          }),
-        // quasar({
-        //     autoImportComponentCase: "combined",
-        //     sassVariables: "./resources/css/quasar-variables.sass",
-        // }),
+	}),
     ],
 });
-
-
-
