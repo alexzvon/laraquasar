6b71deaa5276921d63db9b4ba6e6494d93be2992
ZvonLexa <zvon.lexa@yandex.ru>
2025-03-10 17:10:36 +0300

    update TypeEnum

---
+++ app/Attributes/Enums/Label.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Attributes\Enums;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS_CONSTANT)]
+final readonly class Label
+{
+  public function __construct(
+    public string $label
+  )
+  {}
+}
+
--- app/Enums/TypeEnum.php
+++ app/Enums/TypeEnum.php
@@ -4,44 +4,75 @@ declare(strict_types=1);
 
 namespace App\Enums;
 
-use App\Contracts\HasEnumName;
-use App\Enums\Traits\EnumValue;
-use App\Enums\Traits\EnumName;
+use App\Attributes\Enums\Label;
+use App\Attributes\Enums\Table;
+use App\Traits\Enums\LabelEnum;
+use App\Traits\Enums\TableEnum;
 
-enum TypeEnum: int implements HasEnumName
+// use App\Contracts\HasEnumName;
+// use App\Enums\Traits\EnumValue;
+// use App\Enums\Traits\EnumName;
+
+enum TypeEnum: int  // implements HasEnumName
 {
-  use EnumValue;
+  // use EnumValue;
+    use LabelEnum, TableEnum;
 
-  case Boolean = 1;
-  case Integer = 2;
-  case Brand = 3;
-  case Color = 4;
-  case Dimension = 5;
+    #[Table('')]
+    #[Label('Булево')]
+    case Boolean = 1;
 
-  public static function getEnumName(): string
-  {
-      return TypeNameEnum::class;
-  }
+    #[Table('')]
+    #[Label('Число')]
+    case Integer = 2;
+  
+    #[Table('brands')]
+    #[Label('Справочник Брэнд')]
+    case Brand = 3;
+
+    #[Table('colors')]
+    #[Label('Справочник Цвет')]
+    case Color = 4;
+
+    #[Table('dimensions')]
+    #[Label('Справочник Размер')]
+    case Dimension = 5;
+
+    public static function getOptions(): array
+    {
+        $options = [];
+
+        foreach(self::cases() as $item) {
+            $options[] = [ 'label' => $item->label(), 'value' => $item->value ];
+        }
+
+        return $options;
+    }
+
+    public function getOption(): array
+    {
+        return [ 'label' => $this->label(), 'value' => $this->value ];
+    }
 }
 
-enum TypeNameEnum: string
-{
-    use EnumName;
+// enum TypeNameEnum: string
+// {
+//     use EnumName;
 
-    case Boolean = 'Булево';
-    case Integer = 'Число';
-    case Brand = 'Справочник Брэнд';
-    case Color = 'Справочник Цвет';
-    case Dimension = 'Справочник Размер';
-}
+//     case Boolean = 'Булево';
+//     case Integer = 'Число';
+//     case Brand = 'Справочник Брэнд';
+//     case Color = 'Справочник Цвет';
+//     case Dimension = 'Справочник Размер';
+// }
 
-enum TypeTableEnum: string
-{
-  use EnumName;
+// enum TypeTableEnum: string
+// {
+//   use EnumName;
 
-  case Boolean = '';
-  case Integer = '';
-  case Brand = 'brands';
-  case Color = 'colors';
-  case Dimension = 'dimensions';
-}
+//   case Boolean = '';
+//   case Integer = '';
+//   case Brand = 'brands';
+//   case Color = 'colors';
+//   case Dimension = 'dimensions';
+// }
