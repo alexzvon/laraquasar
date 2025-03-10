1c267e5e8ee91e488115927d7ef2c02d63637fc6
ZvonLexa <zvon.lexa@yandex.ru>
2025-03-10 15:30:58 +0300

    update icons dashboardlayout

---
+++ resources/js/Pages/Dashboard/Products/ListCategoryProducts.vue
@@ -0,0 +1,9 @@
+<script setup>
+  import DashboardLayout from '@/Layouts/DashboardLayout.vue'
+</script>
+
+<template>
+  <DashboardLayout>
+    <h1>List Products</h1>
+  </DashboardLayout>
+</template>
